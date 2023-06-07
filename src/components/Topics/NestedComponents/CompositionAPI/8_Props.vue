49b1c02ddf3133e57bd00e63a528db3bf360fe58
anmol-singh-proxima <kumaranmol888@gmail.com>
2023-06-07 22:49:26 +0530

    Composition API: Props

---
+++ src/components/Topics/NestedComponents/CompositionAPI/8_Props.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Here is one ParentComponent and a ChildComponent, ParentComponent will pass the props to ChildComponent and ChildComponent will display it. This all will be done using Composition API.</p>
+        <ParentComponent />
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+import ParentComponent from './Props/ParentComponent.vue';
+export default {
+    name: 'Props',
+    components: {
+        ParentComponent,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Topics/NestedComponents/CompositionAPI/Props/ParentComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="parent-component">
+        <h4>ParentComponent</h4>
+        <div>
+            <p>The data from these inputs will be passed as props to ChildComponent</p>
+            <input type="text" placeholder="First Name" v-model="firstname" />
+            <input type="text" placeholder="Last Name" v-model="lastname" />
+            <input type="text" placeholder="Hero Name" v-model="heroname" />
+        </div>
+        <ChildComponent :firstname="firstname" :lastname="lastname" :heroname="heroname" />
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+import { ref, reactive, toRefs } from 'vue'
+import ChildComponent from './ChildComponent.vue';
+export default {
+    name: 'ParentComponent',
+    components: {
+        ChildComponent,
+    },
+    setup() {
+        const firstname = ref('Anmol')
+        const lastname = ref('Singh')
+        const heroname = ref('Shera')
+        return {
+            firstname,
+            lastname,
+            heroname
+        }
+    }
+}
+</script>
+
+<style scoped>
+.parent-component {
+    border: 1px solid #999;
+    padding: 10px;
+}
+.parent-component h4, .parent-component p {
+    padding: 6px;
+    margin: 0;
+}
+</style>
